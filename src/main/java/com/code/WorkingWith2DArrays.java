789df136b3eae70934f1758c4f5ed4158ccd9df2
ikechukwu <gospelihueze@gmail>
2023-10-04 13:18:38 +0100

    second commit

---
+++ src/main/java/com/code/WorkingWith2DArrays.java
@@ -0,0 +1,20 @@
+package com.code;
+
+import java.util.Arrays;
+
+public class WorkingWith2DArrays {
+    public static void main(String[] args){
+        char[][] board = new char[3][3];
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j <3 ; j++) {
+                board[i][j] = '-';
+            }
+        }
+        System.out.println(Arrays.deepToString(board));
+    }
+    char[][] boardTwo = new char[][] {
+            new char[] {'-', '-', '-'},
+            new char[] {'-', '-', '-'},
+            new char[] {'-', '-', '-'},
+    };
+}
