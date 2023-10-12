44d9998ec59033a8059abbe69578ceca457c26f9
ikechukwu <gospelihueze@gmail>
2023-10-12 21:25:27 +0100

    initial

--- src/main/java/com/code/Dsa/TwoSum.java
+++ src/main/java/com/code/Dsa/TwoSum.java
@@ -1,9 +1,14 @@
 package com.code.Dsa;
 
+import java.util.Arrays;
 import java.util.HashMap;
 
 public class TwoSum {
-    public int[] twoSum(int[] nums, int target) {
+    public static void main(String[] args) {
+        int[] newArrays = {2, 7, 11, 15};
+        System.out.println(Arrays.toString(twoSum(newArrays, 9)));
+    }
+    public static int[] twoSum(int[] nums, int target) {
         HashMap<Integer, Integer> numMap = new HashMap<>();
 
         for (int i = 0; i < nums.length; i++) {
@@ -11,12 +16,11 @@ public class TwoSum {
 
             if (numMap.containsKey(complement)) {
                 return new int[]{
-                        numMap.get(complement), i};
+                        numMap.get(complement), i
+                };
             }
-
             numMap.put(nums[i], i);
         }
-
         throw new IllegalArgumentException("No two sum solution");
 
     }
